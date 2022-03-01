7f9a53b01620522f4044bb2f094db59e54069158
MalCaor <xav.lemen@orange.fr>
2022-03-01 11:23:46 +0100

    la co

--- client_ftp/client_ftp/Main.java
+++ client_ftp/client_ftp/Main.java
@@ -1,7 +1,23 @@
 package client_ftp;
 
+import java.net.Socket;
+
 public class Main {
+    static Socket socket;
     public static void main(String[] args) throws Exception {
+        // set sock
         System.out.println("client_ftp");
+        // print we here
+        try {
+            socket = new Socket("localhost", 2121);
+        } catch (Exception e) {
+            // no serveur
+            System.out.println("aucun serveur dispo");
+            System.out.println("arret du programe");
+            return;
+        }
+
+        // close sock
+        socket.close();
     }
 }
