c030d258288410dbd3ac414fc68c21931fa4e0f1
Narrateur <theo.paste@outlook.fr>
2022-03-11 17:00:08 +0100

    commande cd

--- ftpprojet/src/main/java/ftpprojet/CommandeCD.java
+++ ftpprojet/src/main/java/ftpprojet/CommandeCD.java
@@ -1,4 +1,5 @@
 package ftpprojet;
+import java.io.File;
 import java.io.PrintStream;
 
 public class CommandeCD extends Commande {
@@ -8,7 +9,39 @@ public class CommandeCD extends Commande {
 	}
 
 	public void execute() {
-		ps.println("La commande cd n'est pas encoré implémentée");
+		
+
+		File f = new File(serveur.currentPath);
+		
+		if(f.isDirectory()) {
+			if(commandeArgs[0].equals("..")){
+				String[] tab = commandeArgs[0].split("/");
+				if(tab[tab.length - 1].equals("personne")){
+					ps.println("2 Erreur, dossier racine atteint");
+				}else{
+					String tempPath=tab[0];
+					for(int i=1; i<tab.length-1; i++){
+						tempPath+="/"+tab[i];
+					}
+					serveur.currentPath=tempPath;
+				}
+			}else{
+				String[] listOfFile = f.list();
+				boolean found = false;
+
+				for (String file : listOfFile) {
+					if(commandeArgs[0].toLowerCase().equals(file)) found = true;
+				}
+				
+				if(found){
+					serveur.currentPath += "/"+commandeArgs[0];
+					ps.println("0 "+serveur.currentPath);
+				}
+			}
+			
+		}else{
+			ps.println("2 cd : Impossible de trouver le chemin d'accès «"+commandeArgs[0]+"», car il n'existe pas.");
+		}
 	}
 
 }
