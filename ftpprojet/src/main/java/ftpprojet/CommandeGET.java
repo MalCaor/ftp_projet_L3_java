187713a5e6f325c3cc35bccf6997725620a7c94a
Narrateur <theo.paste@outlook.fr>
2022-03-11 17:43:34 +0100

    commande GET debut

--- ftpprojet/src/main/java/ftpprojet/CommandeGET.java
+++ ftpprojet/src/main/java/ftpprojet/CommandeGET.java
@@ -1,4 +1,6 @@
 package ftpprojet;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class CommandeGET extends Commande {
@@ -9,6 +11,22 @@ public class CommandeGET extends Commande {
 
 	public void execute() {
 		ps.println("La commande get n'est pas encoré implémentée");
+		File f = new File(serveur.currentPath);
+		if(f.isFile()){
+			File newFile = new File(serveur.clientPath+"/"+commandeArgs[0]);
+			try {
+				if(newFile.createNewFile()){
+					
+				}else{
+					ps.println("2 get : le fichier existe deja");
+				}
+				ps.println("0 get : "+commandeArgs[0]+" téléchargé");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else{
+			ps.println("2 get : "+commandeArgs[0]+" n'est pas un fichier");
+		} 
 	}
 
 }
