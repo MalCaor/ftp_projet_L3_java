6c56695bd28dbdd2d711fcd307ac1a7434b3fd76
MalCaor <xav.lemen@orange.fr>
2022-03-11 11:21:23 +0100

    ui

--- ftpprojet/src/main/java/ftpprojet/client.java
+++ ftpprojet/src/main/java/ftpprojet/client.java
@@ -1,6 +1,8 @@
 package ftpprojet;
 
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -30,6 +32,11 @@ public class client {
 
         System.out.println("connexion etablie");
         BufferedWriter os = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(br.readLine());
+        }
 
         // boucle de communication
         while (communication) {
@@ -40,6 +47,7 @@ public class client {
             os.write(message);
             os.newLine();
             os.flush();
+            System.out.println(br.readLine());
         }
 
         // close os
