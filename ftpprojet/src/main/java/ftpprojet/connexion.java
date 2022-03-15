d8bbbfbaf14ec27a71aa85cbb01c3e27f7fc7955
MalCaor <xav.lemen@orange.fr>
2022-03-15 15:36:44 +0100

    ui

---
+++ ftpprojet/src/main/java/ftpprojet/connexion.java
@@ -0,0 +1,47 @@
+package ftpprojet;
+
+import java.net.Socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class connexion implements Runnable {
+    ServerSocket serveurFTP;
+    Socket socket;
+    connexion(ServerSocket serveur){
+        serveurFTP = serveur;
+    }
+
+    @Override
+	public void run() {
+		try {
+			socket = serveurFTP.accept();
+		
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintStream ps = new PrintStream(socket.getOutputStream());
+			
+			ps.println("1 Bienvenue ! ");
+			ps.println("1 Serveur FTP Personnel.");
+			ps.println("0 Authentification : ");
+			
+			String commande = "";
+			
+			// Attente de reception de commandes et leur execution
+			while(!(commande=br.readLine()).equals("bye")) {
+				System.out.println(">> "+commande);
+				CommandExecutor.executeCommande(ps, commande);
+			}
+			
+			serveurFTP.close();
+			socket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- ftpprojet/src/main/java/ftpprojet/serveur.java
+++ ftpprojet/src/main/java/ftpprojet/serveur.java
@@ -1,47 +1,37 @@
 package ftpprojet;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 
-public class serveur {
+public class serveur{
 	static String clientPath = System.getProperty("user.dir") + "/client";
 	static String userPath = "/personne";
 	static String currentPath = System.getProperty("user.dir") + "/home" + userPath;
 	static String pseudoTMP;
+	static ServerSocket serveurFTP;
 	// dictionaire
 	static HashMap<String, String> listClient = new HashMap<String, String>();
 	// users connected
 	static HashMap<String, String> pathUser= new HashMap<String, String>();
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception  {
 		System.out.println("Le Serveur FTP");
 
 		// init client
 		listClient.put("personne", "abcd");
 		
-		ServerSocket serveurFTP = new ServerSocket(2121);
-		Socket socket = serveurFTP.accept();
-		
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		PrintStream ps = new PrintStream(socket.getOutputStream());
-		
-		ps.println("1 Bienvenue ! ");
-		ps.println("1 Serveur FTP Personnel.");
-		ps.println("0 Authentification : ");
-		
-		String commande = "";
-		
-		// Attente de reception de commandes et leur execution
-		while(!(commande=br.readLine()).equals("bye")) {
-			System.out.println(">> "+commande);
-			CommandExecutor.executeCommande(ps, commande);
+		serveurFTP = new ServerSocket(2121);
+		while(true){
+			connexion con = new connexion(serveurFTP);
+			Thread th = new Thread(con);
+			th.start();
 		}
-		
-		serveurFTP.close();
-		socket.close();
 	}
+
+	
+	
 }
