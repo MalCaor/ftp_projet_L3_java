5b92f037f28695d0c2d10420d0499127dbf4fa4b
MalCaor <xav.lemen@orange.fr>
2022-03-15 14:02:07 +0100

    now with error

--- ftpprojet/src/main/java/ftpprojet/CommandExecutor.java
+++ ftpprojet/src/main/java/ftpprojet/CommandExecutor.java
@@ -9,21 +9,40 @@ public class CommandExecutor {
 	public static void executeCommande(PrintStream ps, String commande) {
 		if(userOk && pwOk) {
 			// Changer de repertoire. Un (..) permet de revenir au repertoire superieur
-			if(commande.split(" ")[0].equals("cd")) (new CommandeCD(ps, commande)).execute();
-	
+			if(commande.split(" ")[0].equals("cd")) 
+			{
+				(new CommandeCD(ps, commande)).execute();
+				return;
+			}
 			// Telecharger un fichier
-			if(commande.split(" ")[0].equals("get")) (new CommandeGET(ps, commande)).execute();
-			
+			if(commande.split(" ")[0].equals("get")) 
+			{
+				(new CommandeGET(ps, commande)).execute();
+				return;
+			}
 			// Afficher la liste des fichiers et des dossiers du repertoire courant
-			if(commande.split(" ")[0].equals("ls")) (new CommandeLS(ps, commande)).execute();
-		
+			if(commande.split(" ")[0].equals("ls")) 
+			{
+				(new CommandeLS(ps, commande)).execute();
+				return;
+			}
 			// Afficher le repertoire courant
-			if(commande.split(" ")[0].equals("pwd")) (new CommandePWD(ps, commande)).execute();
-			
+			if(commande.split(" ")[0].equals("pwd")) 
+			{
+				(new CommandePWD(ps, commande)).execute();
+				return;
+			}
 			// Envoyer (uploader) un fichier
-			if(commande.split(" ")[0].equals("stor")) (new CommandeSTOR(ps, commande)).execute();
-							}
-		else {
+			if(commande.split(" ")[0].equals("stor")) 
+			{
+				(new CommandeSTOR(ps, commande)).execute();
+				return;
+			}
+
+			// no such command
+			ps.println("2 La commande n'existe pas");
+			return;
+		} else {
 			if(commande.split(" ")[0].equals("pass") || commande.split(" ")[0].equals("user")) {
 				// Le mot de passe pour l'authentification
 				if(commande.split(" ")[0].equals("pass")) (new CommandePASS(ps, commande)).execute();
